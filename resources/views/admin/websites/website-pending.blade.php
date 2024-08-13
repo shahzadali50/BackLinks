2dcce8b7b0e32d381fb46cc0a3893ebfa21b5a00
Shahzad Ali <108119561+shahzadali50@users.noreply.github.com>
2024-08-14 01:40:57 +0500

    admin dashboard

--- resources/views/admin/websites/website-pending.blade.php
+++ resources/views/admin/websites/website-pending.blade.php
@@ -34,6 +34,7 @@
             <div class="card">
                 <div class="card-header ">
                     <h3 class="">Pending Websites List</h3>
+                    
 
                 </div>
                 <div class="card-body">
